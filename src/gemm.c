ac7d8778adf828b0aeba83231c7763aa44a0d9b2
Sasha <alexander.drozd@gmail.com>
2021-03-05 15:06:52 +0900

    fix the mess

--- src/gemm.c
+++ src/gemm.c
@@ -4,7 +4,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
-#include <cblas.h>
+#include <cblas.>h
 
 void gemm_bin(int M, int N, int K, float ALPHA, 
         char  *A, int lda, 
@@ -124,17 +124,25 @@ void gemm_tn(int M, int N, int K, float ALPHA,
     }
 }
 
+
 void gemm_tt(int M, int N, int K, float ALPHA, 
         float *A, int lda, 
         float *B, int ldb,
         float *C, int ldc)
 {
-    cblas_sgemm(CblasRowMajor,
-                (TA) ? CblasTrans : CblasNoTrans,
-                (TB) ? CblasTrans : CblasNoTrans,
-                M, N, K, ALPHA, *A, lda,
-                *B, ldb, beta, *C, ldc)
+    int i,j,k;
+    #pragma omp parallel for
+    for(i = 0; i < M; ++i){
+        for(j = 0; j < N; ++j){
+            register float sum = 0;
+            for(k = 0; k < K; ++k){
+                sum += ALPHA*A[i+k*lda]*B[k+j*ldb];
+            }
+            C[i*ldc+j] += sum;
+        }
+    }
 }
+
 
 
 void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA, 
@@ -144,20 +152,17 @@ void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA,
         float *C, int ldc)
 {
     //printf("cpu: %d %d %d %d %d %f %d %d %f %d\n",TA, TB, M, N, K, ALPHA, lda, ldb, BETA, ldc);
-    int i, j;
-    for(i = 0; i < M; ++i){
-        for(j = 0; j < N; ++j){
-            C[i*ldc + j] *= BETA;
-        }
-    }
-    if(!TA && !TB)
-        gemm_nn(M, N, K, ALPHA,A,lda, B, ldb,C,ldc);
-    else if(TA && !TB)
-        gemm_tn(M, N, K, ALPHA,A,lda, B, ldb,C,ldc);
-    else if(!TA && TB)
-        gemm_nt(M, N, K, ALPHA,A,lda, B, ldb,C,ldc);
-    else
-        gemm_tt(M, N, K, ALPHA,A,lda, B, ldb,C,ldc);
+    // int i, j;
+    // for(i = 0; i < M; ++i){
+    //     for(j = 0; j < N; ++j){
+    //         C[i*ldc + j] *= BETA;
+    //     }
+    // }
+    cblas_sgemm(CblasRowMajor,
+                (TA) ? CblasTrans : CblasNoTrans,
+                (TB) ? CblasTrans : CblasNoTrans,
+                M, N, K, ALPHA, *A, lda,
+                *B, ldb, BETA, *C, ldc)
 }
 
 #ifdef GPU
