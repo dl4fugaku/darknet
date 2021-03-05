3e2ab9ebfccfcc072a97531bf49444528e731cf2
Sasha <alexander.drozd@gmail.com>
2021-03-05 15:09:02 +0900

    use pointers to alphabeta

--- src/gemm.c
+++ src/gemm.c
@@ -158,11 +158,14 @@ void gemm_cpu(int TA, int TB, int M, int N, int K, float ALPHA,
     //         C[i*ldc + j] *= BETA;
     //     }
     // }
+    const float * alpha = &ALPHA;
+    const float * beta = &BETA;
+
     cblas_sgemm(CblasRowMajor,
                 (TA) ? CblasTrans : CblasNoTrans,
                 (TB) ? CblasTrans : CblasNoTrans,
-                M, N, K, ALPHA, *A, lda,
-                *B, ldb, BETA, *C, ldc)
+                M, N, K, alpha, *A, lda,
+                *B, ldb, beta, *C, ldc)
 }
 
 #ifdef GPU
